7e0538cd407ec5a7a19f37e81856430d280ada07
hiimchinh <chinhhoang1196@gmail.com>
2019-06-06 10:14:49 +0700

    show login, create component and connect with App

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <Header/>
+    <b-container class="bv-example-row">
+      <b-row >
+        <b-col sm="6" offset="3">
+          <LoginContent/>
+        </b-col>
+      </b-row>
+    </b-container>
+    <Footer/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+  import Header from './components/Header';
+  import Footer from './components/Footer';
+  import LoginContent from './components/LoginContent';
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    LoginContent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+
+  <div>
+    <b-nav pills>
+      <b-nav-item active>Active</b-nav-item>
+      <b-nav-item>Link</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style scoped>
+
+</style>
